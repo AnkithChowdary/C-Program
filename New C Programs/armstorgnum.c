ada3299a6d1f5a31ee00f92657f95bfe9eebe9dc
AnkithChowdary <ankithc29@gmail.com>
2023-10-03 22:16:23 +0530

    Update

--- New C Programs/armstorgnum.c
+++ New C Programs/armstorgnum.c
@@ -2,7 +2,7 @@
 #include<math.h>
 void main()
 {
-    int org,num,rem,re=0,n;
+    int org,num,rem,re=0,n;  // Method 1
     scanf("%d",&num);
     org=num;
     n=0;
@@ -22,4 +22,25 @@ void main()
     {
         printf("Armstrong");
     }else{printf("Not arm");}
+}
+#include<stdio.h>           //Method 2
+#include<math.h>
+#include<stdlib.h>
+
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    int res=0;
+    int org=n;
+    int k=log10(n)+1;      // counts no of digits in a number you can also use lke ceil(log10(n))
+    while(org!=0){
+        int digit=org%10;
+        res+=pow(digit,k);
+        org/=10;
+    }
+    if(res==n)
+        printf("%d is an Armstrong number",n);
+    else
+        printf("%d is not an Armstrong number",n);
 }
