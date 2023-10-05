ef6abfadc71dc1ce413e88405b8fe86125e856cc
AnkithChowdary <ankithc29@gmail.com>
2023-10-05 23:48:53 +0530

    Update

--- Learnvern C Programs/staticStorage.c
+++ Learnvern C Programs/staticStorage.c
@@ -11,6 +11,6 @@ int prin()
 {
    static int k=5;    // It retains it's value and stored in ram and it's initial value is zero and it's lifetime is through out the program but scope is within the block
   k=k+10;
-   printf("%d\n",k);
+   printf("%d\n",k); 
    return 0;
 }
