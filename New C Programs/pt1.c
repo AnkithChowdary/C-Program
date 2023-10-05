e2c2c1762e87aaa029fa2da25d37db2b462bb183
AnkithChowdary <ankithc29@gmail.com>
2023-10-05 10:28:15 +0530

    Update

--- New C Programs/pt1.c
+++ New C Programs/pt1.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 void main()
 {
+
   int n;
   scanf("%d",&n);
+
+  
+  for(int i=0; i<n; i++)
+  {
+    for(int j=1; j<=n-i; j++)
+    {
+      printf(" ");
+    }
+    for(int k=1; k<=2*i-1; k++)
+    {
+      printf("*");
+    }
+
+    printf("\n");
+  }
   for(int i=1;i<=n ;i++)
   {
     for(int j=1; j<=i; j++)
@@ -42,8 +58,25 @@ for(int i=1; i<=n ;i++)
   }
   for(int k=i; k>=1; k--)
   {
-    printf("%d",i);
+    printf("%d",i);                      
   }
   printf("\n");
 }
+ 
+  for(int i=1; i<=10; i++)  
+  {
+    for(int j=1; j<=10; j++)
+    printf("%dx%d=%d\n",i,j,i*j);
+  }
+   for(int i=1; i<=n; i++)    // Odd numbers
+   {
+    for(int j=1; j<=i; j++)
+    {
+      printf("%d",j++);
+    }
+    printf("\n");
+   }
+   
+
 }
+
