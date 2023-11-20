6e0e4f06296e57ec09b2848792928d320483a738
Ankith Chowdary <ankithc29@gmail.com>
2023-11-21 00:56:41 +0530

    Update

---
+++ Data structures/Structures.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+struct persons
+{
+  char name[15];
+  int id;
+  char schoolName[11];
+  char FatherName[14];
+  int exp;
+};
+
+int main()
+{
+    int n;
+    printf("Enter the number of persons: ");
+    scanf("%d", &n);
+  
+    struct persons arr[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("Enter your Name: ");
+        scanf("%s", arr[i].name);
+        printf("Enter your id: ");
+        scanf("%d", &arr[i].id);
+        printf("Enter your school Name: ");
+        scanf("%s", arr[i].schoolName);
+        printf("Enter your experience: ");
+        scanf("%d", &arr[i].exp);
+    }
+
+    printf("\nName\tID\tSchool\tExperience\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%s\t%d\t%s\t%d\n", arr[i].name, arr[i].id, arr[i].schoolName, arr[i].exp);
+    }
+
+    return 0;
+}
