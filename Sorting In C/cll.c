80385cddb1dd5a6f72b4861dc6c3ef9ae89127d7
Ankith <ankithc29@gmail.com>
2024-02-28 10:23:41 +0530

    Update

---
+++ Sorting In C/cll.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+  int n;
+  struct node *next;
+}*head=NULL,*newnode,*ptr,*p;
+
+void create()
+{
+  newnode=(struct node*)malloc(sizeof(struct node));
+  scanf("%d",&(newnode->n));
+  newnode->next=NULL;
+  if(head==NULL)
+  head=ptr=newnode;
+  else{
+    ptr->next=newnode;
+    ptr=newnode;
+
+  }
+  newnode->next=head;
+}
+void display(){
+p=head;
+do{
+  printf("%d->",p->n);
+p=p->next;
+}while(p!=head);
+
+}
+
+
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  while(n-->0)
+    create();
+    display();
+  
+}
